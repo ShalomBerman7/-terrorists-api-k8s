92d0be5c2fea5e3cdde9573cf641f608efab57d6
shalom berman 7 <bermanshalom7@gmail.com>
2026-01-15 11:47:20 +0200

    Endpoint construction, testing and validation

---
+++ models.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+
+
+def file_handling(df):
+    df = df.sort_values(by=["danger_rate"], ascending=[False])
+    return selecting_the_top(df)
+
+
+def selecting_the_top(df):
+    df = df.head()
+    return validation(df)
+
+
+class Person(BaseModel):
+    name: str
+    location: str
+    danger_rate: int = Field(..., ge=1, le=10)
+
+
+def validation(df):
+    validated = []
+    for i, row in df.iterrows():
+        person = Person(name=row["name"], location=row["location"], danger_rate=int(row["danger_rate"])
+                        )
+        validated.append(person)
+    response = {"count": len(validated), "top": [person.dict() for person in validated]
+                }
+    return response
